52742c38968c43caf169570549f22c03b9191834
MrTin0 <tinocasaleiro@gmail.com>
2023-06-18 03:51:52 +0200

    added folders for better nav

--- Code/delta-app.lua
+++ Code/delta-app.lua
@@ -1,5 +1,5 @@
 --DELTA APP
-local Source_url = "https://raw.githubusercontent.com/MrTin0/Delta-App/main/main.lua"
+local Source_url = "https://raw.githubusercontent.com/MrTin0/Delta-App/main/Code/main.lua"
 TABLE_VERSION = 1
 local http = getInternet()
 Source = http.getURL(Source_url)
