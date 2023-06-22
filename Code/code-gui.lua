46c130a29823757393eab46af7d6da9685550555
MrTin0 <tinocasaleiro@gmail.com>
2023-06-23 00:20:40 +0200

    update

--- Code/code-gui.lua
+++ Code/code-gui.lua
@@ -7,7 +7,7 @@ function createUI()
       UI.Width = 500
       UI.ActiveControl = Steer
       UI.BorderStyle = bsSingle
-      UI.Caption = "DeltaApp by Vi'o'Lence 3" 
+      UI.Caption = "DeltaApp by Vi'o'Lence 4" 
       UI.ClientHeight = 214
       UI.ClientWidth = 500
       UI.Color = clGrayText
