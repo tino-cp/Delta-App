01cdb04ff7a3b72ec9f3a5af918fa5af3546acb5
MrTin0 <tinocasaleiro@gmail.com>
2023-07-14 15:10:54 +0200

    JSON UPDATE

--- Code/main.lua
+++ Code/main.lua
@@ -1289,8 +1289,9 @@ function RequireIncomingTransaction(Amount, Reason)
   local TrackName = readString('adr + E5AF0')
   local data = json.encode(details)
   local Username = "[[[WorldPTR]+pCPed]+pCPlayerInfo]+oName"
-
-  https.postURL(TransactionURL,"Track="..TrackName.."&Player="..Username.."&S1="..S1.."&S2="..S2.."&S3="..S3)
+  local S3_raw = CurLapLastCheckpointTime-S1_raw-S2_raw
+  details = {}
+  https.postURL(TransactionURL,"Track="..TrackName.."&Player="..Username.."&S1="..S1_raw.."&S2="..S2_raw.."&S3="..S3_raw)
   https.destroy()
 end
 
