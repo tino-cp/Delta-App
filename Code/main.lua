7318adc9afb076e48a5dd8927ef62bd77b8c32b5
MrTin0 <tinocasaleiro@gmail.com>
2023-08-02 14:57:35 +0200

    Lap senmding fix

--- Code/main.lua
+++ Code/main.lua
@@ -202,12 +202,6 @@ function NewLapProcedure()
   if CurCheckpoint == 0 and LastCheckpoint ~= 0 and CurLapLastCheckpointTime ~= 0 then
     CurrentLapSectors[0] = CurLapLastCheckpointTime
     --LOGS
-    if CanWrite == true and isLapSet == false then
-      isLapSet = true
-      RequireIncomingTransaction()
-      CanWrite = false
-    end
-
     if LogsEnabled == true and CanWrite==true then
       --Record laptime
       LogsLaptime = CurLapLastCheckpointTime
@@ -235,6 +229,15 @@ function NewLapProcedure()
       LatestFastLapSectors = FastLapSectors[0]
       for i=0,MaxCheckpoints-1 do
         FastLapSectors[i] = CurrentLapSectors[i]
+      end
+    end
+
+    --LOGS ONLINE
+    if CanWrite == true and isLapSet == false then
+      isLapSet = true
+      if S1_raw > 0 and S2_raw > 0 and S3_raw > 0 then
+        RequireIncomingTransaction()
+        CanWrite = false
       end
     end
   end
