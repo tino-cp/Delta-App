880e3d1d40a7e0c6afd5639ce063f009cede7993
Valentino Casaleiro Perez <v.casaleiroperez@student.han.nl>
2024-02-08 16:40:50 +0100

    Update UI caption and print checkpoints

--- Code/main.lua
+++ Code/main.lua
@@ -476,25 +476,29 @@ function FindAdr()
 end
 
 function FindTimes()
-  print("Checkpoint 1/3")
+  print("Checkpoint 1/6")
   Enable.Caption = "Scanning memory 2/3"
-  print("Checkpoint 2/3")
+  print("Checkpoint 2/6")
   local results = AOBScan('FF FF FF FF 00 00 00 00 00 00 00 00 ?? 0? 00 00 08 00 00 00 00 00 00 00 05 00 00 00 00 00 00 00 ?? ?? ?? ?? 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 EC FF FF FF ?? 0?', '-X-C+W', 2, '8')
-  print("Checkpoint 3/3")
+  print("Checkpoint 3/6")
 
   assert(#results > 0, 'aobscan failed') -- Checking if any results were found
+  print("Checkpoint 4/6")
 
   -- Since the results are in an array, loop through them to print and process each one
   for i, result in ipairs(results) do
     print("Result " .. i .. ": " .. result)
     -- Do whatever processing you need with the results here
   end
+  print("Checkpoint 5/6")
 
 
   --assert(results, 'aobscan failed')
 
   local addr = results[1]
+  print("Checkpoint 6/6")
   results.destroy()  
+  
   registerSymbol('TimesPTR',addr)
 end
 
