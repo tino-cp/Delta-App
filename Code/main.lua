ae345ab14628b74a17e7132d4e565cb5be405deb
Valentino Casaleiro Perez <v.casaleiroperez@student.han.nl>
2024-03-01 15:48:40 +0100

    Fix StartDetect.OnClick assignment in code-gui.lua and update memory addresses in main.lua

--- Code/main.lua
+++ Code/main.lua
@@ -121,7 +121,7 @@ function ActivateApp()
   previousKersValue = nil
   kersGainedOnLap = 0
   kersUsedOnLap = 0
-  TrackName = readString('adr + 71D5DBF8')
+  TrackName = readString('adr + 4140BAD8')
   nameaddr = "[[[WorldPTR]+pCPed]+pCPlayerInfo]+oName"
 
   Value = createTimer(nil, false)
@@ -137,8 +137,8 @@ function InitTrackInfo()
   S1_raw=0
   S2_raw=0
   S3_raw=0
-  MaxCheckpoints = readInteger('adr') --old CBF40 new D 16C0 del prev 97C60 928
-  local Track_Name = readString('adr + 71D5DBF8')
+  MaxCheckpoints = readInteger('adr + 10') --old CBF40 new D 16C0 del prev 97C60 928
+  local Track_Name = readString('adr + 4140BAD8')
   UI.Caption = Track_Name.." Delta App Online 1.2.7"
   CurLapLastCheckpointTime = 0
   LastCheckpoint = 100
@@ -266,7 +266,7 @@ function UpdateInfo()
     StartDetect.Caption='STOP'
 
     ChecksPTR = getAddress('TimesPTR')
-    ForLogs_TrackName=readString('adr + 71D5DBF8') --new E3998 del 3A388 prev A9610
+    ForLogs_TrackName=readString('adr + 4140BAD8') --new E3998 del 3A388 prev A9610
     if LogsEnabled == true then
       CanWrite=false
     end
@@ -471,8 +471,8 @@ end
 function FindAdr()
   Enable.Caption = "Scanning memory 1/3"
   --local results = AOBScan('02 00 00 00 ?? 0? 00 00 FF FF FF FF 00 00 00 00 00 00 00 00 ?? 0? 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ?? 0? 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00', '*X*C*W', 2, '000')
-  local results = AOBScan('?? 00 00 00 00 00 00 00 0? 00 00 00 ?? 0? 00 00 0? 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 06 00 00 00 00 00 00 00 F6 FF FF FF 00 00 00 00 04 00 00 00 00 00 00 00 77 15 4F 42 00 00 00 00 CD CC CC 3E')
-
+  --local results = AOBScan('?? 00 00 00 00 00 00 00 0? 00 00 00 ?? 0? 00 00 0? 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 06 00 00 00 00 00 00 00 F6 FF FF FF 00 00 00 00 04 00 00 00 00 00 00 00 77 15 4F 42 00 00 00 00 CD CC CC 3E', '*X*C*W', 2, '000')
+  local results = AOBScan('?? ?? 0? 00 00 00 00 00 01 00 00 00 00 00 00 00 ?? 00 00 00 ?? 0? 00 00 ?? 00 00 00 ?? 0? 00 00 0? 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 0? 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF FF FF FF 00 00 00 00 FF FF FF FF', '*X*C*W', 1, '000')
   assert(results, 'aobscan failed')
   local addr = results[0]
   results.destroy()
@@ -483,7 +483,7 @@ end
 function FindTimes()
   Enable.Caption = "Scanning memory 2/3"
 --   local results = AOBScan('FF FF FF FF 00 00 00 00 00 00 00 00 ?? 0? 00 00 08 00 00 00 00 00 00 00 05 00 00 00 00 00 00 00 ?? ?? ?? ?? 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 EC FF FF FF ?? 0?', '-X-C+W', 2, '8')
-  local results = AOBScan('?? ?? ?? 0? 00 00 00 00 01 00 00 00 00 00 00 00 ?? ?? ?? 0? 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ?? ?? ?? 00 ?? ?? 00 00 FF FF FF FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ?? ?? ?? ?? ?? ?? 00 00 ?? ?? ?? 45 00 00 00 00 ?? ?? ?? 42 00 00 00 00 ?? ?? ?? ?? ?? ?? 00 00 ?? 0? ?? 0? ?? ?? 00 00 ?? 0? 00 00 ?? 0?')
+  local results = AOBScan('?? ?? ?? 0? 00 00 00 00 01 00 00 00 00 00 00 00 ?? ?? ?? 0? 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ?? ?? ?? 00 ?? ?? 00 00 FF FF FF FF 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ?? ?? ?? ?? ?? ?? 00 00 ?? ?? ?? 45 00 00 00 00 ?? ?? ?? 42 00 00 00 00 ?? ?? ?? ?? ?? ?? 00 00 ?? 0? ?? 0? ?? ?? 00 00 ?? 0? 00 00 ?? 0?', '-X-C+W', 2, '8')
   assert(results, 'aobscan failed')
   local addr = results[0]
   results.destroy()  
@@ -522,7 +522,7 @@ function LogsSwitcher()
 end
 
 function PackLogs()
-  local ForLogs_TrackName = readString('adr + 71D5DBF8')
+  local ForLogs_TrackName = readString('adr + 4140BAD8')
   local save_dialog = createSaveDialog(self)
   save_dialog.InitalDir = os.getenv('%USERPROFILE%')
   if save_dialog.execute() then
@@ -560,7 +560,7 @@ function SaveFastLap()
   for i=0,MaxCheckpoints-1 do
       FLdata = FLdata..FastLapSectors[i].."\n"
   end
-  local TrackName = readString('adr + 71D5DBF8')
+  local TrackName = readString('adr + 4140BAD8')
   local save_dialog = createSaveDialog(self)
   save_dialog.InitalDir = os.getenv('%USERPROFILE%')
   if save_dialog.execute() then
@@ -587,7 +587,7 @@ function LoadFastLap()
      if load_dialog.execute() then
      file = io.open(load_dialog.FileName, "r")
      local Track = tostring(file:read())
-     if Track == readString('adr + 71D5DBF8') then
+     if Track == readString('adr + 4140BAD8') then
         for i=0,MaxCheckpoints-1 do
             FastLapSectors[i] = tonumber(file:read())
         end
@@ -598,8 +598,8 @@ end
 
 function Startup()
   Enable.Enabled = false
-  --FindAdr()
-  --FindTimes()
+  FindAdr()
+  FindTimes()
   FindCar()
 
   ActivateApp()
@@ -1282,7 +1282,7 @@ end
 local runOnce = false
 
 function ReadTrackName()
-  local TrackNameNew = readString('adr + 71D5DBF8')
+  local TrackNameNew = readString('adr + 4140BAD8')
   if Enable == true then
     if TrackNameNew ~= '' and TrackNameNew ~= TrackName then
       if not runOnce then
