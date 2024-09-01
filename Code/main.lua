1bd75eef5f74e6462520ac388b98613d8cd988c6
Valentino Casaleiro Perez <v.casaleiroperez@student.han.nl>
2024-09-01 16:54:31 +0200

    ver

--- Code/main.lua
+++ Code/main.lua
@@ -145,7 +145,7 @@ function InitTrackInfo()
   MaxCheckpoints = readInteger('GTA5.exe+26A435C')
   --MaxCheckpoints = readInteger('GTA5.exe+26A32DC') -- 1  --old CBF40 new D 16C0 del prev 97C60 928
   local Track_Name = readString('GTA5.exe+2017E70')--readString('adr + 6051E150')
-  UI.Caption = Track_Name.." - Delta App Online 1.2.10"
+  UI.Caption = Track_Name.." - Delta App Online 1.2.11"
   CurLapLastCheckpointTime = 0
   LastCheckpoint = 100
   CurrentLapSectors = {}
@@ -261,7 +261,7 @@ function UpdateInfo()
     LoadFLButton.Enabled = false
     LogBuildingButton.Enabled = false
 
-    UI.Caption = "Delta App Online 1.2.10"
+    UI.Caption = "Delta App Online 1.2.11"
   elseif Enable == false then
     getAdr()
     UpdateCar()
